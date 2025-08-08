87caa87aa23290d7f434200e20fdb19396844a9d
Ayaat2012 <iffatrajshahi@gmail.com>
2025-08-08 19:47:04 +0600

    initial commit

---
+++ L2-A2.py
@@ -0,0 +1,10 @@
+setx = {"black", "pink"}
+sety = {"purple", "black"}
+print("Original set of elements :")
+print(setx)
+print(sety)
+
+# Intersection of sets
+print("\nThe intersection of two said sets :")
+setz = sety.intersection(setx)
+print(setz)
---
+++ L2-A4.py
@@ -0,0 +1,10 @@
+setx = {"blue", "white"}
+sety = {"white", "black"}
+print("Original set of elements :")
+print(setx)
+print(sety)
+
+# Union of sets
+print("\nThe union of two said sets :")
+setz = sety.union(setx)
+print(setz)
