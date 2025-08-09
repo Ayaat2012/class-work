e35a27be417443b555adbcb194655d1a687b3c26
Ayaat2012 <iffatrajshahi@gmail.com>
2025-08-09 19:58:51 +0600

    initial commit

---
+++ L3-A4.py
@@ -0,0 +1,21 @@
+class Parrot:
+
+    # Instance attributes
+    # Constructor method - special method
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Instance method
+    def sing(self, song):
+        return ("{} sings {}".format(self.name, song))
+    
+    def dance(self):
+        return ("{} is now dancing".format(self.name))
+    
+# Instantiate the object
+blu = Parrot("blu", 10)
+
+# Call our instance methods
+print(blu.sing(" 'Happy' "))
+print(blu.dance())
